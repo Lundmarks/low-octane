5673738f64690ad67bc5b4a09cbe7f920980d90f
ldmk@outlook.com <ldmk@outlook.com>
2021-01-07 15:56:35 +0100

    First commit for Low Octane Forum

---
+++ README.md
@@ -0,0 +1,12 @@
+# Low Octane@GitHub
+## CI
+
+//To be implemented
+
+## About Low Octane
+
+Low Octane is a forum created for and by fuel enthusiasts. The forum is containing discussions regarding leaded and unleaded fuel, and where to find different types of fuel. The forum also discusses fuel qualities, prices and the horror that is electric vehicles driving the prices up.
+
+## Installation
+
+//To be written
---
+++ config.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'lowoctanedb',
+    'username' => 'lundmark',
+    'password' => 'password',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://localhost:8080/dbwebb/ramverk1/me/kmom10/low-octane/public',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
